20bc004b9e6a42fc6bd2ef8bb3833e6989b42988
VinCodeTheWeb <vincenzo33.pellegrini@gmail.com>
2020-01-06 01:44:40 +0700

    Adding Start script

---
+++ src/middleware/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findOne({
+      _id: decoded._id,
+      'tokens.token': token
+    });
+
+    if (!user) {
+      throw new Error({ error: 'Access denied.' });
+    }
+
+    req.user = user;
+    req.token = token;
+
+    next();
+  } catch (e) {
+    res.status(401).send(e);
+  }
+};
+
+module.exports = auth;
